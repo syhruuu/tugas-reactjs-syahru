9e34af980ab610e95103d55c46e56a2a1c6ba10b
syhruuu <muhammad90635@smk.belajar.id>
2024-05-03 15:12:12 +0700

    tugas

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="box">
+        <h1 className="judul">Hi, Saya Syahru</h1>
+        <hr></hr>
+        <p className="paragraf">
+          Saya dari Kelas 12 sudah berhasil menginstall React
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
